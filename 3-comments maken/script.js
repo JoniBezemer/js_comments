6532e94815a592db510995ac2cbe4edf25efa40a
JoniBezemer <77683711+JoniBezemer@users.noreply.github.com>
2021-03-25 13:42:21 +0100

    Add files via upload

---
+++ 3-comments maken/script.js
@@ -0,0 +1,12 @@
+console.log("Hallo Winc Academy studenten")
+
+//Dit is een grote som
+
+/*let multiplication=1230941*1823794;
+
+let division=637263 / 54;*/
+
+//Ik zie de uitkomst van mijn comments niet in mijn console. Omdat de comments niet worden gezien als geldige code
+//omdat er deze schuine streepjes voor staan.
+
+//Ik zie de uitkomsten van de sommen niet meer omdat ze uitgecomment zijn.
